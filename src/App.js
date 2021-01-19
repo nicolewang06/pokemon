e039b841f589782cb21965042b94b13c82b91a60
Nicole Wang <nicolewang06@gmail.com>
2021-01-18 22:46:10 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-// installed axios - npm i axios. axios makes HTTP requests (calls)
-  // import axios
-// installed react router - npm install react-router-dom. router generates urls
-  // import react-router-dom
-  import React from 'react';
-  import axios from 'axios';
-  import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-  import './App.css'
-  import PokeCard from './PokeCard'
-  
-  //const WINES_URL = 'http://myapi-profstream.herokuapp.com/api/779738/wines/'
-  //const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon/'
-  const PEOPLE_URL = 'http://myapi-profstream.herokuapp.com/api/fdf5d8/persons/'
+import React from 'react';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css'
   
   const sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
   const img = "https://pokeres.bastionbot.org/images/pokemon/"
@@ -26,19 +17,19 @@
   
       this.state = {};
     }
-  
-  
+
     render() {
       return (
       <Router>
         <div className="header">
           <div className="nav">
-            <Link to="/home">Home</Link> || <Link to="/Pokedex">Pokedex</Link> || <Link to="/pokemon">Pokemon</Link>
+            <Link to="/home">Home</Link>&nbsp; || &nbsp;<Link to="/Pokedex">Pok&#233;dex</Link>&nbsp; || &nbsp;<Link to="/pokemon">Pok&#233;mon</Link>
           </div>
-  
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
+
           <Switch>
+            <Route exact path="/">
+              <Pokedex />
+            </Route>
             <Route path="/pokedex">
               <Pokedex />
             </Route>
@@ -65,16 +56,9 @@
       
         async componentDidMount() {
           try{
-            //const res = await axios.get(POKEMON_URL);
-            //const response = await axios.get('http://hp-api.herokuapp.com/api/characters')
-            //const response = await axios.get('https://pokeapi.co/api/v2/pokemon/')
-            const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=3')
+            const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
             //console.log(res)
             this.setState( { data: res.data.results } )
-            //console.log(res.data)
-            //const id=(res.data.results[0])
-            //console.log(id)
-            //console.log(response.data)
             
           } catch(error) {
             console.error(error.message);
@@ -83,7 +67,7 @@
 
         render() { 
           return ( 
-            <div>
+            <div id="holder">
                {this.state.data.map(pokemon => 
              <GetAllPokemon
                 name={pokemon.name} 
@@ -105,9 +89,6 @@
           try{
           const {data} = await axios.get(this.props.url)
           this.setState ( { data: data } )
-          data.types.forEach( (type) => {
-            pokemon['type'] = pokemon['type'] + ', ' + type.type.name;
-          })
         } catch(error) {
           console.error(error.message);
         }
@@ -121,20 +102,32 @@
               <div className="pokemonCard">           
 
                   <div className="pokemonBackground"  key={this.state.data.id}>
-                    <img src= {img + this.state.data.id + ".png"} alt="image" width="180px" />
+                    <a href="pokedex"><img id="pokemonImage" src= {img + this.state.data.id + ".png"} alt="" width="100px" /></a>
                   </div> 
 
                   <div className="pokemonContent">
-                  <h2 id="pokemonName">#{this.state.data.id} {this.state.data.name}</h2>
+                  <h3 id="pokemonName">#{this.state.data.id} {this.state.data.name}</h3>
 
-                  <h5>Ability: {this.state.data.abilities && this.state.data.abilities[0].ability.name}</h5>
-                  <h6>Move: {this.state.data.moves && this.state.data.moves[0].move.name}</h6>
+                  
+                  {/* <h6>Move: {this.state.data.moves && this.state.data.moves.map(move => {
+                    console.log(move);
+                    return(<span>{move.move.name},</span>);
+                  })}</h6>  
 
+                  <h6>Type: { this.state.data.types && this.state.data.types.map(type => {
+                      console.log(type);
+                      return (<span>{type.type.name}, </span>);
+                    }) }
+                  </h6>
+                  <h6>{this.state.data.types && this.state.data.types[0].type.name}</h6>*/}
+ {/* <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      /> */}
                       
                   </div>
 
               </div>
-
             </div>
           )
         }
@@ -146,104 +139,189 @@
       super(props);
   
       this.state = { data: [] }
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
   
+    handleChange(e) {
+      const { name, value } = e.target;
+      this.setState ( { [name]: value } );
+    }
+
+    async handleSubmit(e) {
+      e.preventDefault();
+      const pokemon = this.state.search;
+
+      try{
+        const resInput = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        this.setState( { data: resInput.data } )
+
+      } catch(er) {
+        console.error(er.message);
+      }
+    }
+
     async componentDidMount() {
       try{
-        //const res = await axios.get(POKEMON_URL);
-        //const response = await axios.get('http://hp-api.herokuapp.com/api/characters')
         const res = await axios.get('https://pokeapi.co/api/v2/pokemon/133')
-        //const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
         console.log(res)
         this.setState( { data: res.data } )
-        //console.log(res.data.sprites.front_default)
-        //const pokeID = res.data.results.url;
-        //console.log(pokeID)
       } catch(error) {
         console.error(error.message);
       }
     }
+
+
+
   
     render() { 
       return ( 
-         <div className = "pokemonHeader">
-          <input type="text" name="pokemonSearch" placeholder="enter # or name"/>
+        <div className="pokedexPageContainer">
 
-           <div className = "pokemonCards">
-              <div className = "cardContent">
-                <h1>{this.state.data.id}. {this.state.data.name}</h1>
-                <img src= {sprite + this.state.data.id + ".png"} alt="images" />
-                <h3>Height: {this.state.data.height}</h3>
-                <h3>Weight: {this.state.data.weight}</h3>
+          <div className = "pokedexSearchBar">
+            <form 
+                id="searchBar"
+                onChange={this.handleChange}
+                onClick={this.handleSubmit}>
+                  <input type="text" name="search" id="search" placeholder="enter Pok&#233;mon name or # between 1 - 898" size="42" />
+                  &nbsp;&nbsp;<input type="submit" id="pokedexSearch" value="who's that Pok&#233;mon ?"  />
+            </form> 
+          </div>
+
+           <div className="pokedex">
+             {/* ------ left side of pokedex -------- */}
+              <div className="leftSide">
+
+                <div className="topLightsContainer">
+                    <div id="blueLight"></div>
+                    <div id="blueDark"></div>
+                    <div id="blueHighlight"></div>
+                    <div id="redLight"></div>
+                    <div id="yellowLight"></div>
+                    <div id="greenLight"></div>
+                 </div>
+
+                 <div id="topLine"></div>
+                 <div id="topLine2"></div>
+
+              <div className="imgContainer">
+                <div id="redDot1"></div>
+                <div id="redDot2"></div>
+                <div className="imgDisplay">
+                  <img src= {sprite + this.state.data.id + ".png"} alt=""  />
+                </div>
+                <div id="bottomLeftDot"></div>
+
+                <div id="bottomRightLine1"></div>
+                <div id="bottomRightLine2"></div>
+                <div id="bottomRightLine3"></div>
               </div>
-           </div>
 
-         </div>
+              <div id="bottomCorner"></div>
+
+              <div id="blkLeftButton"></div>
+              <div id="redLine"></div>
+              <div id="blueLine"></div>
+              <div id="greenDisplay"><p id="pokedexID"># {this.state.data.id}</p></div>
+              <div className="dPad">
+                <div id="up">
+                  <div id="upButton"></div>
+                </div>
+                <div id="right">
+                <div id="rightButton"></div>
+                </div>
+                <div id="left">
+                <div id="leftButton"></div>
+                </div>
+                <div id="down">
+                <div id="downButton"></div>
+                </div>
+              </div>
+
+              </div>
+
+              {/* ------ pokedex divider -------- */}
+              <div className="middleDivider">
+                <div id="hinge1"></div>
+                <div id="hinge2"></div>
+              </div>
+
+              {/* ------ right side of pokedex -------- */}
+              <div className="rightSide">
+
+                  <div id="topRightLine"></div>
+                  <div id="topRightLine2"></div>
+                  <div id="blankSection"></div>
+                  <div id="blankSection2"></div>
+                  <div id="blankSection3"></div>
+
+                <div id="detailsBox"><p id="pokedexName">{this.state.data.name}</p></div>
+                  <div className="blueButtonContainer">
+                    <div className="topBlueButtonRow">
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                    </div> 
+                    <div className="bottomBlueButtonRow">
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                      <div className="blueButton"></div>
+                    </div>  
+                  </div>  
+
+                    <div className="blackSideButtonContainer">
+                      <div id="blackSideButton1"></div>
+                      <div id="blackSideButton2"></div>
+                    </div>
+                    <div className="whiteButtonContainer">
+                      <div id="whiteButton1"></div>
+                      <div id="whiteButton2"></div>
+                    </div>
+
+                    <div className="yellowRightCircle">
+                      <div id="yellowRightCircleDark"></div>
+                      <div id="yellowRightCircleHighlight"></div>
+                    </div>
+
+                    <div className="pokedexBottomRight">
+                      <div id="leftBottom">Height: {this.state.data.height}</div>
+                      <div id="rightBottom">Weight: {this.state.data.weight}</div>
+                    </div>
+
+                  </div> 
+              
+           </div>{/* closing pokedex */}
+
+        </div>/* closing pokedexPageContainer */
+          
        );
     }
   }
 
+class Home extends React.Component{
+  constructor(props){
+    super(props);
 
+    this.state = {}
 
-
-
-
-  
-  class Home extends App {
-    constructor() {
-      super();
-      this.state = {
-        pokemons : [],
-        pokemonDetails : [],
-      }    
-    }
-  
-    componentDidMount() {
-      this.getMorePokemon();
-    }
-  
-    getMorePokemon() {
-      //let url = "https://pokeapi.co/api/v2/pokemon?offset=" + this.state.offset + "&limit=" + this.state.loadNumber;
-      let url = "https://pokeapi.co/api/v2/pokemon?limit=151"
-      fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        if (data) {
-          this.setState({pokemons : data.results}, () => {
-            this.state.pokemons.map(pokemon => {
-              fetch(pokemon.url)
-              .then(response => response.json())
-              .then(data => {
-                if (data) {
-                  var temp = this.state.pokemonDetails
-                  temp.push(data)
-                  this.setState({pokemonDetails: temp})
-                }            
-              })
-              .catch(console.log)
-            })
-          })        
-        }
-      })
-      .catch(console.log)
-    }
-  
-    render() {
-      const {pokemonDetails} = this.state;
-  
-      const renderedPokemonList = pokemonDetails.map((pokemon, index) => {
-        return (<PokeCard pokemon={pokemon} />);
-      });
-  
-      return (
-        <div className="container2">
-          <div className="card-columns">
-            {renderedPokemonList}
-          </div>
-        </div>
-      );
-    }
-  }
     
+  }
+
+
+
+render() {
+  return(
+    <div>
+      <h3>who's that pokemon game</h3>
+    </div>
+  );
+}
+}
+
   
+
   export default App;
---
+++ src/PokemonCards.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function PokemonCards() {
+    const img = "https://pokeres.bastionbot.org/images/pokemon/";
+  return(
+    <div className="pokemonCardContainer">
+              
+              <div className="pokemonCard">           
+
+                  <div className="pokemonBackground"  key={this.state.data.id}>
+                    <a href="pokedex"><img src= {img + this.state.data.id + ".png"} alt="" width="120px" /></a>
+                  </div> 
+
+                  <div className="pokemonContent">
+                  <h3 id="pokemonName">#{this.state.data.id} {this.state.data.name}</h3>
+
+                  
+                  {/* <h6>Move: {this.state.data.moves && this.state.data.moves.map(move => {
+                    console.log(move);
+                    return(<span>{move.move.name},</span>);
+                  })}</h6>  
+
+                  <h6>Type: { this.state.data.types && this.state.data.types.map(type => {
+                      console.log(type);
+                      return (<span>{type.type.name}, </span>);
+                    }) }
+                  </h6>
+                  <h6>{this.state.data.types && this.state.data.types[0].type.name}</h6>*/}
+ {/* <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      /> */}
+                      
+                  </div>
+
+              </div>
+            </div>
+  )
+}
+
+
